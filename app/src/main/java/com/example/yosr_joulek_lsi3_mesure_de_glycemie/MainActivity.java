dca738a1d1c6251e9c5474562f532937368d07d2
yosr <joulekyosr123@gmail.com>
2023-10-14 21:33:01 +0100

    first commit

---
+++ app/src/main/java/com/example/yosr_joulek_lsi3_mesure_de_glycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yosr_joulek_lsi3_mesure_de_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Yosr_Joulek_LSI3_Mesure_De_Glycemie"
+include(":app")
+ 
